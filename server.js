5a60ddf7e230c1d8ab9af8f02d99856e38c08f18
Sai Teja Suggula <suggulasaiteja@gmail.com>
2021-02-03 18:00:50 +0530

    Add Deployment Files

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("./dist/meeto"));
+
+app.get("/*", function (req, res) {
+  res.sendFile("index.html", { root: "dist/meeto" });
+});
+
+app.listen(process.env.PORT || 4200);
